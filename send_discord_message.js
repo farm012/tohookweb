e113692c0b4e59963b863e6551fe44d33a2a3137
farm <66478784+farm012@users.noreply.github.com>
2024-05-01 20:31:58 +0200

    Update send_discord_message.js

--- send_discord_message.js
+++ send_discord_message.js
@@ -1,24 +1,21 @@
-const fetch = require('node-fetch');
+const got = require('got');
 
 // Replace 'YOUR_WEBHOOK_URL' with your actual Discord webhook URL
 const WEBHOOK_URL = 'https://discord.com/api/webhooks/1235292044721782939/sJA4AKOgA9-Uze7FfvfLxsDQGos3eqfbPEOFuGGZKmT8KqL-lEjcnocvyUT1jhWJQElZ';
 
 async function sendDiscordMessage(message) {
     try {
-        const response = await fetch(WEBHOOK_URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
+        const response = await got.post(WEBHOOK_URL, {
+            json: {
                 content: message,
-            }),
+            },
+            responseType: 'json',
         });
 
-        if (response.ok) {
+        if (response.statusCode === 204) {
             console.log('Message sent successfully');
         } else {
-            console.error(`Failed to send message. Status code: ${response.status}`);
+            console.error(`Failed to send message. Status code: ${response.statusCode}`);
         }
     } catch (error) {
         console.error('An error occurred while sending the message:', error);
@@ -27,7 +24,10 @@ async function sendDiscordMessage(message) {
 
 async function main() {
     // Replace 'Your message here' with your desired message
-    await sendDiscordMessage('Your message here');
+    setInterval(() => {
+        sendDiscordMessage('Your message here');
+    }, 2000); // Send message every 2 seconds
 }
 
 main();
+
